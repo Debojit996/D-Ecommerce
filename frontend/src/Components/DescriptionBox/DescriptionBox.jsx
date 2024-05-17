7b41637e8905f05c95b3caa30db2df01563ada1c
Debojit Dwari <dwaridebojit3@gmail.com>
2024-05-17 14:38:33 +0530

    first commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './DesciptionBox.css'
+
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="description-navigator">
+          <div className="descriptionbox-nav-box">Description</div>
+          <div className="descriptionbox-nav-box review">Reviews(224)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>
+              Here's a description for an e-commerce clothing website:
+              Welcome to <span>D-Shop</span> , your premier destination for stylish and affordable fashion online! At <span>D-Shop</span>, we curate a diverse collection of clothing and accessories to suit every taste, occasion, and budget.
+              Explore our extensive range of trendy apparel for men, women, and kids, carefully selected from top brands and emerging designers alike. From casual essentials to statement pieces, we have everything you need to refresh your wardrobe and express your unique style.
+              Discover the latest fashion trends and timeless classics in our easy-to-navigate online store. Whether you're searching for the perfect outfit for a special event, updating your everyday essentials, or simply treating yourself to something new, we've got you covered.
+              Shop with confidence knowing that we prioritize quality, affordability, and customer satisfaction. Our secure checkout process and flexible payment options make it quick and convenient to place your order, while our dedicated customer support team is here to assist you every step of the way.</p>
+            <p>Experience the joy of online shopping with <span>D-Shop</span>. With fast shipping, hassle-free returns, and exciting promotions year-round, it's never been easier to elevate your style and shop for the latest fashion trends from the comfort of your own home.
+              Join our community of fashion enthusiasts today and discover why <span>D-Shop</span> is the ultimate destination for fashion-forward individuals everywhere!
+              Feel free to customize this description to fit the unique branding and offerings of your e-commerce clothing website.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
